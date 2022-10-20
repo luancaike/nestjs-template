e19ed445b4a4f6d73e5b57425d2641d5fff3dda3
Luan Caike <luan.matos@grupomateus.com>
2022-10-20 19:42:27 -0300

    init project

---
+++ src/database/database.providers.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const dataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION as any,
+  host: process.env.TYPEORM_HOST,
+  port: +(process.env.TYPEORM_PORT || 0),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  migrations: [__dirname + '/migrations/*{.ts,.js'],
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: false,
+});
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      return dataSource.initialize();
+    },
+  },
+];
